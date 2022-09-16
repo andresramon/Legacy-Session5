14725e21cb6756bce4206430e7b49f6e402f431d
Andres Ramon <andres.ramon@gmail.com>
2022-09-16 15:36:54 +0200

    Move GetCategory to player

--- Trivia/Player.cs
+++ Trivia/Player.cs
@@ -59,4 +59,19 @@ public class Player
         Console.WriteLine(_name + " was sent to the penalty box");
         inPenaltyBox = true;
     }
+
+    public string GetCategory()
+    {
+        int playersPlace = this.GetLocation();
+        if (playersPlace == 0) return "Pop";
+        if (playersPlace == 4) return "Pop";
+        if (playersPlace == 8) return "Pop";
+        if (playersPlace == 1) return "Science";
+        if (playersPlace == 5) return "Science";
+        if (playersPlace == 9) return "Science";
+        if (playersPlace == 2) return "Sports";
+        if (playersPlace == 6) return "Sports";
+        if (playersPlace == 10) return "Sports";
+        return "Rock";
+    }
 }
--- Trivia/Players.cs
+++ Trivia/Players.cs
@@ -6,9 +6,7 @@ namespace Trivia;
 public class Players
 {
     private int _currentPlayer=0;
-
     private readonly List<Player> _players = new List<Player>();
-
 
     public Player currentPlayer => _players[_currentPlayer];
     public String currentPlayerName => currentPlayer.ToString();
@@ -34,17 +32,7 @@ public class Players
  
     public string CurrentCategory()
     {
-        int playersPlace = currentPlayer.GetLocation();
-        if (playersPlace == 0) return "Pop";
-        if (playersPlace == 4) return "Pop";
-        if (playersPlace == 8) return "Pop";
-        if (playersPlace == 1) return "Science";
-        if (playersPlace == 5) return "Science";
-        if (playersPlace == 9) return "Science";
-        if (playersPlace == 2) return "Sports";
-        if (playersPlace == 6) return "Sports";
-        if (playersPlace == 10) return "Sports";
-        return "Rock";
+        return currentPlayer.GetCategory();
     }
 
     public void CorrectlyAnswered()
@@ -54,8 +42,7 @@ public class Players
 
     public void NextPlayerTurn()
     {
-        _currentPlayer++;
-        if (_currentPlayer == this._players.Count) _currentPlayer = 0;
+        if (++_currentPlayer == this._players.Count) _currentPlayer = 0;
     }
 
     public bool IsCurrentPlayerInPenaltyBox()
